304efae1eda7433c728fce2a35a2da03ebce7e35
Hasib Hossain <boraborhasib@gmail.com>
2023-03-23 17:12:52 +0600

    NavBar File Added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NavBar.css';
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <div className='navbar__logo'>
+                <h2>Marsi.</h2>
+            </div>
+            <div>
+                <ul className='navbar__menu'>
+                    <li><i className="fa-brands fa-dribbble"></i> Dribble</li>
+                    <li><i className="fa-brands fa-behance"></i> Behance</li>
+                    
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import NavBar from './components/NavBar/NavBar';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar></NavBar>
   </React.StrictMode>
 );
 
